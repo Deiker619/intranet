b716fdd24a7889534143f6bfa85bb70225d464e7
Deiker Fernández <deiker1842@gmail.com>
2025-03-21 09:39:06 -0400

    fix: bug en los estilos en el responsive

--- resources/views/livewire/mysql/modal.blade.php
+++ resources/views/livewire/mysql/modal.blade.php
@@ -200,7 +200,7 @@
                                 </div>
 
                             </button>
-                            <button data-modal-hide="static-modal" type="button"
+                            <button data-modal-hide="static-modal" type="button" wire:click='$set("show", false)' 
                                 class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Cancelar</button>
                         </div>
 
--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -142,8 +142,12 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
+           
             <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link href="{{ route('constancia') }}" :active="request()->routeIs('constancia')">
+                {{ __('Constancia') }}
             </x-responsive-nav-link>
         </div>
 
